1351ef84eee9c9a5bda1b45f1e5804c41cab2ee4
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-09-12 10:08:30 +0530

    Some testing with postman

--- controller/authController.js
+++ controller/authController.js
@@ -7,9 +7,15 @@ module.exports.login_get = (req, res) => {
 };
 
 module.exports.sugnup_post = (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
   res.send("new signup");
 };
 
 module.exports.login_post = (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
   res.send("user login");
 };
